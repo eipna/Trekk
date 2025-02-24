919935f614cfd7f6c5e1ebccc8d2194ea95603b0
Eipna <Xirveipna@tuta.io>
2025-02-24 13:36:30 +0800

    Setup edge-to-edge and view bindings in main activity

--- app/src/main/java/com/eipna/trekk/MainActivity.java
+++ app/src/main/java/com/eipna/trekk/MainActivity.java
@@ -1,14 +1,21 @@
 package com.eipna.trekk;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.eipna.trekk.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
     }
 }
